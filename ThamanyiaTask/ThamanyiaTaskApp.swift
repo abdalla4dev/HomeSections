d0b4a3631acd0e4d27e0eafcd2c77268454d8e10
abdallah ragab <aragab-e@jahez.net>
2024-11-23 11:56:28 +0300

    Initial Commit

---
+++ ThamanyiaTask/ThamanyiaTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThamanyiaTaskApp.swift
+//  ThamanyiaTask
+//
+//  Created by abdallah ragab on 23/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThamanyiaTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThamanyiaTaskTests/ThamanyiaTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThamanyiaTaskTests.swift
+//  ThamanyiaTaskTests
+//
+//  Created by abdallah ragab on 23/11/2024.
+//
+
+import Testing
+@testable import ThamanyiaTask
+
+struct ThamanyiaTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
